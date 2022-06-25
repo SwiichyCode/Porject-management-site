adfc515e43582141d4afe4f12f54d5e8db4a1841
Swiichy <adlpromail@gmail.com>
2022-06-25 18:36:36 +0200

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQTVKfsCl2MUTSClrn5syJ40ptOYuliH4",
+  authDomain: "thedojosite-51952.firebaseapp.com",
+  projectId: "thedojosite-51952",
+  storageBucket: "thedojosite-51952.appspot.com",
+  messagingSenderId: "1019066111227",
+  appId: "1:1019066111227:web:388ce5d9b39ec0a8c38c5e",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
